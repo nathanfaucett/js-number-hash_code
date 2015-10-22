1f652f2c18360a8ec67a1203e768c7e6ea93b870
nathanfaucett <nathanfaucett@gmail.com>
2015-10-22 08:07:35 -0500

    updates

--- test/index.js
+++ test/index.js
@@ -4,5 +4,9 @@ var tape = require("tape"),
 
 tape("numberHashCode(number: Number) should return hash code for number", function(assert) {
     assert.equals(numberHashCode(0), 0);
+    assert.equals(numberHashCode(-32), -32);
+    assert.equals(numberHashCode(1234.1234), 529996656);
+    assert.equals(numberHashCode(1231), 1231);
+    assert.equals(numberHashCode(1237), 1237);
     assert.end();
 });
